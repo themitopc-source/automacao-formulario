180b2cc9b88e01069b0de93427098ffe3c7d277f
themitopc-source <themitopc@gmail.com>
2025-08-26 10:51:55 -0300

    Estrutura inicial do projeto Flask para Render

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def home():
+    if request.method == 'POST':
+        nome = request.form.get('nome')
+        email = request.form.get('email')
+        return f"<h2>Formulário recebido!</h2><p>Nome: {nome}</p><p>Email: {email}</p>"
     return render_template('index.html')
-
-if __name__ == '__main__':
-    app.run(debug=True)
